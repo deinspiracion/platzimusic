c35e4fc0c1fdf2c56b40390ae8edfd9e6d6f96ef
deinspiracion <alejandro.onate.c@gmail.com>
2018-06-20 00:22:23 -0400

    initial commit

---
+++ api/config.js
@@ -0,0 +1,8 @@
+export default {
+    appName:"platzi music",
+    apiKey:"76b2f5b39d758d8c066b9ab0b6c00e0d",
+    secret:'666ac046f7bd54bffd1d1e8b0278440a',
+    registeredTo:'deinspiracion'
+
+
+}
---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 Platzimusc
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" :value="country.value") {{country.name}}
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+    
+
+</template>
+
+<script>
+import Artist from './components/Artist'
+import getArtists from '../api'
+import spinner from './components/loading.vue'
+import { logicalExpression } from 'babel-types';
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists:[],
+      countries:[
+        {name:'Argentina',value:"argentina"},
+        {name:'Colombia',value:"colombia"},
+        {name:'España',value:"spain"}
+      ],
+      selectedCountry:'argentina',
+      loading:true
+    }
+  },
+  components:{
+    Artist,
+    spinner
+  },
+  methods:{
+    refreshArtist(){
+       const self = this
+       this.loading=true;
+        this.artists=[];
+        getArtists(this.selectedCountry).then( function(artists){
+          self.loading=false
+          self.artists= artists
+        } )
+    }
+  },
+  mounted:function(){
+    console.log("mount");
+    
+    this.refreshArtist()
+  },
+  watch:{
+    
+    
+    selectedCountry:function(){
+      console.log("watch");
+      this.refreshArtist()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color red !important
+  margin-top 60px
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>
